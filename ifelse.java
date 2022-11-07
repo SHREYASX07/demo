021eeca9b321e17e944bafe5a0a28488c990ff97
SHREYASX07 <117010042+SHREYASX07@users.noreply.github.com>
2022-11-07 10:12:09 +0530

    Java pgm

---
+++ ifelse.java
@@ -0,0 +1,16 @@
+public class ifelse
+{
+public static void main(String args[])
+{
+int a=3,b=6;
+		if(a>b) 
+		{
+		
+			System.out.println("a is greater than b");
+		}
+		 else
+			   {		   
+			System.out.println("a is smaller than b");
+			   }
+}
+}
